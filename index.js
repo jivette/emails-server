3c3a181e837e6f2e45b927ba921a47a0d8f4ff26
Ivette <combinando.ideas@gmail.com>
2019-09-13 14:47:40 -0600

    add module body parser  in index
    add module body parser in index

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const express = require("express");
 const routes = require('./routes');
 const app = express();
 
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
 const cors = require('cors')
 app.use(cors())
 
